c61ac57c103e8c90fedb8f7ce34442e88c8e804f
Msadeler <maya.sadeler@gmail.com>
2024-08-23 15:10:49 +0200

    spcify model used for generation and test

--- function_test.py
+++ function_test.py
@@ -4,11 +4,11 @@ import scipy
 def F(mark,t,psi):
     return(1- np.exp( -psi*mark ))
 
-def phi(mark, gamma, psi):
-    return((psi-gamma)/psi*np.exp(mark*gamma))
-
 def f(t,mark, psi):
     return(psi*np.exp(-psi*mark))
+
+def phi(mark, gamma, psi):
+    return((psi-gamma)/psi*np.exp(mark*gamma))
 
 def phi1(mark, gamma, psi):
     return(psi**(gamma)/scipy.special.gamma(gamma+1)*mark**(gamma) )
